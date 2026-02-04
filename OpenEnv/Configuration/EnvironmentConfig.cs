889231b68593243a907412cafb1711a409a523d2
Ethan Batt <Ethan.Batt@camcoeng.com.au>
2026-02-05 07:35:31 +0800

    Add Service to Inject Environment Configurations

---
+++ OpenEnv/Configuration/EnvironmentConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OpenEnv.Configuration
+{
+
+    public class SqlCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class NetworkCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Domain { get; set; }
+    }
+
+    public class EnvironmentConfig
+    {
+        public string ServerIP { get; set; }
+        public SqlCredentials SqlCredentials { get; set; }
+        public NetworkCredentials NetworkCredentials { get; set; }
+    }
+
+    public class AppConfig
+    {
+        public Dictionary<string, EnvironmentConfig> Environments { get; set; }
+    }
+}
---
+++ OpenEnv/Configuration/EnvironmentConfigService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace OpenEnv.Configuration
+{
+    public class EnvironmentConfigService
+    {
+        private const string _productionKey = "Production";
+        private const string _testingKey = "Testing";
+        private const string _developmentKey = "Development";
+
+        private readonly AppConfig _config;
+        public EnvironmentConfigService(IOptions<AppConfig> config)
+        {
+            _config = config.Value;
+        }
+
+        public EnvironmentConfig GetEnvironmentConfig(string key)
+        {
+            if (!_config.Environments.ContainsKey(key))
+            {
+                throw new KeyNotFoundException($"{key} environment configuration not found.");
+            }
+
+            var env = _config.Environments[key];
+
+            return new EnvironmentConfig
+            {
+                ServerIP = env.ServerIP,
+                SqlCredentials = env.SqlCredentials,
+                NetworkCredentials = env.NetworkCredentials
+            };
+        }
+
+        /*
+         Register in program.cs: builder.Services.Configure<AppConfig>(builder.Configuration.GetSection("AppConfig"));
+         */
+    }
+}
